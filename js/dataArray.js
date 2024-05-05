b781225e82bfddd985af85a593e96f7d6a137e59
jTech <kietpawpan@gmail.com>
2024-05-05 20:48:16 +0700

    Create dataArray.js

---
+++ js/dataArray.js
@@ -0,0 +1,64 @@
+ var dataArray = 
+
+[
+{
+	"วิชา": "ภาษาไทย", 
+	"การบ้าน": "คัดไทย หน้า 1-10", 
+	"กำหนดส่ง": "16 พ.ค. 66", 
+	"ภาพ": "https://drive.google.com/open?id=136h1sYH5xDe4vOVw64IsJFr--eiiOwfk", 
+	"Status": "yes"}, 
+{
+	"วิชา": "กิจกรรม", 
+	"การบ้าน": "ทำปกหนังสือ", 
+	"กำหนดส่ง": "15 พ.ค. 67", 
+	"ภาพ": "https://drive.google.com/open?id=16dGTvpQd0mHHU1CMwPgPv8gaEv8daryx", 
+	"Status": "no"}, 
+{
+	"วิชา": "ภาษาไทย", 
+	"การบ้าน": "ท่องอาขยาน ลำนำ หน้า 36 https://youtu.be/cCpUwjihF2c", 
+	"กำหนดส่ง": "31 พ.ค. 67", 
+	"ภาพ": "https://drive.google.com/open?id=10jmkB3SDBgsr_2iwNDIl8bCigH7S3gms", 
+	"Status": "no"}, 
+{
+	"วิชา": "กิจกรรม", 
+	"การบ้าน": "ส่งรูปถ่าย 1 นิ้ว 6 ใบ", 
+	"กำหนดส่ง": "31 พ.ค. 67", 
+	"ภาพ": "https://drive.google.com/open?id=1t_XR2g7VR3slLx2mfLnfmwXzPO6hJLYL", 
+	"Status": "no"}, 
+{
+	"วิชา": "กิจกรรม", 
+	"การบ้าน": "ส่งรูปคุณป้า 1 นิ้ว ทำบัตรรับ-ส่ง เขียนชื่อจีรวิชญ์ เลขที่ 16 หลังรูป", 
+	"กำหนดส่ง": "20 พ.ค. 67", 
+	"ภาพ": "https://drive.google.com/open?id=1cA4vCYBpGgdp1SCJOMw0JSLB6wqOTp8m", 
+	"Status": "no"}, 
+{
+	"วิชา": "ลูกเสือ", 
+	"การบ้าน": "ซื้อรองเท้าลูกเสือ", 
+	"กำหนดส่ง": "พ.ค. 67", 
+	"ภาพ": "https://drive.google.com/open?id=15cA3orB7fZbbopf1naW9Vkpv7X58irqn", 
+	"Status": "no"}, 
+{
+	"วิชา": "กิจกรรม", 
+	"การบ้าน": "จ่ายค่าเรียนคาบ 8", 
+	"กำหนดส่ง": "20 พ.ค. 67", 
+	"ภาพ": "https://drive.google.com/open?id=1ld7VD3s80_HOzjXlfCqEN-53JlPnVKXE", 
+	"Status": "no"}, 
+{
+	"วิชา": "คณิตศาสตร์", 
+	"การบ้าน": "ท่องสูตรคุณ แม่ 2-12", 
+	"กำหนดส่ง": "ทุกวัน", 
+	"ภาพ": "https://drive.google.com/open?id=1-qgxQD-dOArio1nmXSX78WJJqQxqD1ZR", 
+	"Status": "no"}, 
+{
+	"วิชา": "Developmental English", 
+	"การบ้าน": "อ่าน Reading Aloud บทที่ 1-5", 
+	"กำหนดส่ง": "ทุกวัน", 
+	"ภาพ": "https://drive.google.com/open?id=1ZgySMMD4pPvCdJwFBfMDdSRWyrrJEl1w", 
+	"Status": "no"}, 
+{
+	"วิชา": "กิจกรรม", 
+	"การบ้าน": "ชำระค่าเทอม ป.4 ภาค 1 5-6 หมื่น", 
+	"กำหนดส่ง": "พ.ค. 67", 
+	"Status": "no"}
+  
+];
