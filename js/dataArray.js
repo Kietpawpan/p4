0a833a2e927e0d9eb8c612b21bb38193bca9287a
jTech (Monte Kietpawpan) <kietpawpan@gmail.com>
2024-05-06 17:36:00 +0700

    Update dataArray.js

--- js/dataArray.js
+++ js/dataArray.js
@@ -1,85 +1,94 @@
- var dataArray = 
+ var dataArray =
 
-[
-{
-	"วิชา": "ภาษาไทย", 
-	"การบ้าน": "คัดไทย หน้า 1-10", 
-	"กำหนดส่ง": "16 พ.ค. 66", 
-	"ภาพ": "https://drive.google.com/open?id=136h1sYH5xDe4vOVw64IsJFr--eiiOwfk", 
-	"Status": "yes"}, 
-{
-	"วิชา": "กิจกรรม", 
-	"การบ้าน": "ทำปกหนังสือ", 
-	"กำหนดส่ง": "15 พ.ค. 67", 
-	"ภาพ": "https://drive.google.com/open?id=16dGTvpQd0mHHU1CMwPgPv8gaEv8daryx", 
-	"Status": "no"}, 
-{
-	"วิชา": "ภาษาไทย", 
-	"การบ้าน": "ท่องอาขยาน ลำนำ หน้า 36 https://youtu.be/cCpUwjihF2c", 
-	"กำหนดส่ง": "31 พ.ค. 67", 
-	"ภาพ": "https://drive.google.com/open?id=10jmkB3SDBgsr_2iwNDIl8bCigH7S3gms", 
-	"Status": "no"}, 
-{
-	"วิชา": "กิจกรรม", 
-	"การบ้าน": "ส่งรูปถ่าย 1 นิ้ว 6 ใบ ใส่ถุง ติดชื่อ", 
-	"กำหนดส่ง": "31 พ.ค. 67", 
-	"ภาพ": "https://drive.google.com/open?id=1t_XR2g7VR3slLx2mfLnfmwXzPO6hJLYL", 
-	"Status": "no"}, 
-{
-	"วิชา": "กิจกรรม", 
-	"การบ้าน": "ส่งรูป ผปค. 1 นิ้ว ทำบัตรรับ-ส่ง เขียนชื่อ เลขที่ นร. หลังรูป", 
-	"กำหนดส่ง": "20 พ.ค. 67", 
-	"ภาพ": "https://drive.google.com/open?id=1cA4vCYBpGgdp1SCJOMw0JSLB6wqOTp8m", 
-	"Status": "no"}, 
-{
-	"วิชา": "ลูกเสือ", 
-	"การบ้าน": "ซื้อรองเท้าลูกเสือ", 
-	"กำหนดส่ง": "พ.ค. 67", 
-	"ภาพ": "https://drive.google.com/open?id=15cA3orB7fZbbopf1naW9Vkpv7X58irqn", 
-	"Status": "no"}, 
-{
-	"วิชา": "กิจกรรม", 
-	"การบ้าน": "จ่ายค่าเรียนคาบ 8", 
-	"กำหนดส่ง": "20 พ.ค. 67", 
-	"ภาพ": "https://drive.google.com/open?id=1ld7VD3s80_HOzjXlfCqEN-53JlPnVKXE", 
-	"Status": "no"}, 
-{
-	"วิชา": "คณิตศาสตร์", 
-	"การบ้าน": "ท่องสูตรคุณ แม่ 2-12", 
-	"กำหนดส่ง": "ทุกวัน", 
-	"ภาพ": "https://drive.google.com/open?id=1-qgxQD-dOArio1nmXSX78WJJqQxqD1ZR", 
-	"Status": "no"}, 
-{
-	"วิชา": "Developmental English", 
-	"การบ้าน": "อ่าน Reading Aloud บทที่ 1-5", 
-	"กำหนดส่ง": "ทุกวัน", 
-	"ภาพ": "https://drive.google.com/file/d/12DMXH_ejWuLjUH-6IixU0wmU3GnrOWS1/view?usp=drivesdk", 
-	"Status": "no"}, 
-{
-	"วิชา": "กิจกรรม", 
-	"การบ้าน": "ชำระค่าเทอม ป.4 ภาค 1 5-6 หมื่น", 
-	"กำหนดส่ง": "พ.ค. 67", 
-	"Status": "no"},
-	
-{
-'วิชา':'กิจกรรม',
-'การบ้าน':'รับบัตรเสริม 2 ใบ ชำระแล้ว',
-'กำหนดส่ง':'พ.ค. 67',
-'Status':'yes'
-},
+ 	[{
+ 			"วิชา": "ภาษาไทย",
+ 			"การบ้าน": "คัดไทย หน้า 1-10",
+ 			"กำหนดส่ง": "16 พ.ค. 66",
+ 			"ภาพ": "https://drive.google.com/open?id=136h1sYH5xDe4vOVw64IsJFr--eiiOwfk",
+ 			"Status": "yes"
+ 		},
+ 		{
+ 			"วิชา": "กิจกรรม",
+ 			"การบ้าน": "ทำปกหนังสือ",
+ 			"กำหนดส่ง": "15 พ.ค. 67",
+ 			"ภาพ": "https://drive.google.com/open?id=16dGTvpQd0mHHU1CMwPgPv8gaEv8daryx",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "ภาษาไทย",
+ 			"การบ้าน": "ท่องอาขยาน ลำนำ หน้า 36 https://youtu.be/cCpUwjihF2c",
+ 			"กำหนดส่ง": "31 พ.ค. 67",
+ 			"ภาพ": "https://drive.google.com/open?id=10jmkB3SDBgsr_2iwNDIl8bCigH7S3gms",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "กิจกรรม",
+ 			"การบ้าน": "ส่งรูปถ่าย 1 นิ้ว 6 ใบ ใส่ถุง ติดชื่อ",
+ 			"กำหนดส่ง": "31 พ.ค. 67",
+ 			"ภาพ": "https://drive.google.com/open?id=1t_XR2g7VR3slLx2mfLnfmwXzPO6hJLYL",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "กิจกรรม",
+ 			"การบ้าน": "ส่งรูป ผปค. 1 นิ้ว ทำบัตรรับ-ส่ง เขียนชื่อ เลขที่ นร. หลังรูป",
+ 			"กำหนดส่ง": "20 พ.ค. 67",
+ 			"ภาพ": "https://drive.google.com/open?id=1cA4vCYBpGgdp1SCJOMw0JSLB6wqOTp8m",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "ลูกเสือ",
+ 			"การบ้าน": "ซื้อรองเท้าลูกเสือ",
+ 			"กำหนดส่ง": "พ.ค. 67",
+ 			"ภาพ": "https://drive.google.com/open?id=15cA3orB7fZbbopf1naW9Vkpv7X58irqn",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "กิจกรรม",
+ 			"การบ้าน": "จ่ายค่าเรียนคาบ 8",
+ 			"กำหนดส่ง": "20 พ.ค. 67",
+ 			"ภาพ": "https://drive.google.com/open?id=1ld7VD3s80_HOzjXlfCqEN-53JlPnVKXE",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "คณิตศาสตร์",
+ 			"การบ้าน": "ท่องสูตรคุณ แม่ 2-12",
+ 			"กำหนดส่ง": "ทุกวัน",
+ 			"ภาพ": "https://drive.google.com/open?id=1-qgxQD-dOArio1nmXSX78WJJqQxqD1ZR",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "Developmental English",
+ 			"การบ้าน": "อ่าน Reading Aloud บทที่ 1-5",
+ 			"กำหนดส่ง": "ทุกวัน",
+ 			"ภาพ": "https://drive.google.com/file/d/12DMXH_ejWuLjUH-6IixU0wmU3GnrOWS1/view?usp=drivesdk",
+ 			"Status": "no"
+ 		},
+ 		{
+ 			"วิชา": "กิจกรรม",
+ 			"การบ้าน": "ชำระค่าเทอม ป.4 ภาค 1 5-6 หมื่น",
+ 			"กำหนดส่ง": "พ.ค. 67",
+ 			"Status": "no"
+ 		},
 
-{
-'วิชา':'กิจกรรม',
-'การบ้าน':'ใช้อีเมล sgcomxxx@student.sg.go.th',
-'กำหนดส่ง':'ทุกวัน',
-'Status':'no'
-},
+ 		{
+ 			'วิชา': 'กิจกรรม',
+ 			'การบ้าน': 'รับบัตรเสริม 2 ใบ ชำระแล้ว',
+ 			'กำหนดส่ง': 'พ.ค. 67',
+ 			'Status': 'yes'
+ 		},
 
-{
-'วิชา':'ผปค',
-'การบ้าน':'ปฐมนิเทศน์',
-'กำหนดส่ง':'1 มิ.ย. 67',
-'Status':'no'
-}
-	
-];
+ 		{
+ 			'วิชา': 'กิจกรรม',
+ 			'การบ้าน': 'ใช้อีเมล sgcomxxx@student.sg.go.th',
+ 			'กำหนดส่ง': 'ทุกวัน',
+ 			'Status': 'no'
+ 		},
+
+ 		{
+ 			'วิชา': 'ผปค',
+ 			'การบ้าน': 'ปฐมนิเทศน์',
+ 			'กำหนดส่ง': '1 มิ.ย. 67',
+ 			'Status': 'no'
+ 		}
+
+ 	];
